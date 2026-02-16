f6178736e16c3dad744bd8cbf7c6ceb4a5b6591d
Alan Brooks <alanbrooks036@gmail.com>
2026-02-16 12:06:38 -0500

    remove: useless member

--- src/config/members.ts
+++ src/config/members.ts
@@ -9,10 +9,12 @@ export const MEMBERS: MemberProfile[] = [
   { id: "423971987208142849", github: "https://github.com/codenerd87" },
   { id: "1426742678506311740", github: "https://github.com/crossjbly" },
   { id: "1042919810469740625", github: "https://github.com/SPIRAME" },
-  { id: "1101547649477386331", github: "https://github.com/ZeglolTheThirtySixth" },
+  {
+    id: "1101547649477386331",
+    github: "https://github.com/ZeglolTheThirtySixth",
+  },
   { id: "821086846636916737", github: "https://github.com/DMDCR" },
   { id: "1426598979302457446", github: "https://github.com/xXMariahScaryXx" },
   { id: "952792525637312552", github: "https://github.com/kxtzownsu" },
   { id: "1264014997063995536", github: "https://github.com/genericness" },
-  { id: "759683999810715648", github: "https://github.com/carbonicality" },
 ];
