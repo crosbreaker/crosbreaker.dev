b43fc8ab22cd21f3a318d3f59e6a44e5e9494a34
akane <107654710+genericness@users.noreply.github.com>
2026-02-18 16:38:42 -0800

    SEO demon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://crosbreaker.dev",
   output: "static",
   build: {
     inlineStylesheets: "auto"
-  }
+  },
+  integrations: [sitemap()]
 });
